9667c64c70d2c3a332114dd796e629cbde21bd86
John Livingston <jliv84@gmail.com>
2017-02-24 16:18:43 +0900

    improved intereactive functionality

--- hoshi/hoshi.py
+++ hoshi/hoshi.py
@@ -14,7 +14,7 @@ class Mamajek:
         self.df['SpT_let'] = [i[0] for i in self.df['SpT']]
         self.n = int(1e4)
 
-    def query(self, color, c, uc, m=None, n=None):
+    def query(self, color="B-V", c=0, uc=1, m=None, n=int(1e4), verbose=True):
 
         if n is None:
             n = self.n
@@ -34,27 +34,39 @@ class Mamajek:
 
         df = self.df.sort_values(by=color)
 
+        out = {}
+
         targ = 'Teff'
         res = np.interp(s, df[color].values, df[targ].values)
-        print "{0} = {1:.0f} +/- {2:.0f}".format(targ, np.nanmean(res), np.nanstd(res))
+        out[targ] = [np.nanmean(res), np.nanstd(res)]
+        msg = "{0} = {1:.0f} +/- {2:.0f}".format(targ, np.nanmean(res), np.nanstd(res))
 
         targ = 'logL'
         res = np.interp(s, df[color].values, df[targ].values)
-        print "{0} = {1:.2f} +/- {2:.2f}".format(targ, np.nanmean(res), np.nanstd(res))
+        out[targ] = [np.nanmean(res), np.nanstd(res)]
+        msg += "\n{0} = {1:.2f} +/- {2:.2f}".format(targ, np.nanmean(res), np.nanstd(res))
 
         targ = 'Msun'
         res = np.interp(s, df[color].values, df[targ].values)
-        print "{0} = {1:.2f} +/- {2:.2f}".format(targ, np.nanmean(res), np.nanstd(res))
+        out[targ] = [np.nanmean(res), np.nanstd(res)]
+        msg += "\n{0} = {1:.2f} +/- {2:.2f}".format(targ, np.nanmean(res), np.nanstd(res))
 
         targ = 'SpT_num'
         res = np.interp(s, df[color].values, df[targ].values)
         spt = np.interp(s, df[color].values, df.index.values)
         spt_let = df['SpT_let'][int(round(np.nanmean(spt)))]
-        print "SpT = {0}{1:.1f}V +/- {2:.1f}".format(spt_let, np.nanmean(res), np.nanstd(res))
+        out["SpT"] = "{0}{1:.1f}V +/- {2:.1f}".format(spt_let, np.nanmean(res), np.nanstd(res))
+        msg += "\nSpT = {0}{1:.1f}V +/- {2:.1f}".format(spt_let, np.nanmean(res), np.nanstd(res))
 
         if m is not None:
             targ = M_str
             idx = ~df[[color,targ]].isnull().any(axis=1)
             res = np.interp(s, df[idx][color].values, df[idx][targ].values)
             d = dist(m, res)
-            print "Dphot = {0:.0f} +/- {1:.0f} pc".format(np.nanmean(d), np.nanstd(d))
+            out["Dphot"] = [np.nanmean(d), np.nanstd(d)]
+            msg += "\nDphot = {0:.0f} +/- {1:.0f} pc".format(np.nanmean(d), np.nanstd(d))
+
+        if verbose:
+            print msg
+
+        return out
